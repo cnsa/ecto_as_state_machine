9777d28a0f9012b8de2ed7b1049e953e8d0b4060
Alexander Merkulov <sasha@merqlove.ru>
2017-09-03 22:02:28 +0300

    Version bump 1.0.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule EctoAsStateMachine.Mixfile do
   use Mix.Project
 
   @project_url "https://github.com/cnsa/ecto_as_state_machine"
-  @version "1.0.1"
+  @version "1.0.2"
 
   def project do
     [
